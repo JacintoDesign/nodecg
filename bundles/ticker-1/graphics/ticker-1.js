40e3619d0b44c2a2ab6c8967b09237859e321fe4
Jacinto Wong <jacinto.wong@cbc.ca>
2023-04-18 18:55:12 -0400

    basic animation in and out

--- bundles/ticker-1/graphics/ticker-1.js
+++ bundles/ticker-1/graphics/ticker-1.js
@@ -1,56 +1,102 @@
-		// We can access Replicants from other bundles by specifying the bundle name as a second parameter.
-		// NodeCG requires that bundle names match their directory names, but you can always check the `package.json` to double check.
-		//const netCBC = 
-		//console.log(netCBC);
-		const headerReplicant = nodecg.Replicant('header');
-		const titleReplicant = nodecg.Replicant('title');
-		const messageReplicant = nodecg.Replicant('message');
-		const lowerReplicant = nodecg.Replicant('lower');
-		var netCBCRep = nodecg.Replicant('netCBC');
-		//console.log(netCBCRep);
-		
 
-			// Change will be called when the Replicant loads too, so we can use it to set the initial value.
-			headerReplicant.on('change', (newValue) => {
-			nav_header.innerText = newValue;
-		});
-			// Change will be called when the Replicant loads too, so we can use it to set the initial value.
-			titleReplicant.on('change', (newValue) => {
-			nav_title.innerText = newValue;
-		});
-			// Change will be called when the Replicant loads too, so we can use it to set the initial value.
-			messageReplicant.on('change', (newValue) => {
-			nav_message.innerText = newValue;
-		});
-			// Change will be called when the Replicant loads too, so we can use it to set the initial value.
-			lowerReplicant.on('change', (newValue) => {
-			nav_lower.innerText = newValue;
-		});
+// const headerReplicant = nodecg.Replicant('header');
+// const titleReplicant = nodecg.Replicant('title');
+// const messageReplicant = nodecg.Replicant('message');
+// const lowerReplicant = nodecg.Replicant('lower');
+// var netCBCRep = nodecg.Replicant('netCBC');		
 
-		netCBCRep.on('change', (newValue) => {
-			netCBCRep.value = newValue;
-			console.log("CBC change", netCBCRep.value)
-		});	
+// // Change will be called when the Replicant loads too, so we can use it to set the initial value.
+// headerReplicant.on('change', (newValue) => {
+//     nav_header.innerText = newValue;
+// });
+// // Change will be called when the Replicant loads too, so we can use it to set the initial value.
+// titleReplicant.on('change', (newValue) => {
+//     nav_title.innerText = newValue;
+// });
+// // Change will be called when the Replicant loads too, so we can use it to set the initial value.
+// messageReplicant.on('change', (newValue) => {
+//     nav_message.innerText = newValue;
+// });
+// // Change will be called when the Replicant loads too, so we can use it to set the initial value.
+// lowerReplicant.on('change', (newValue) => {
+//     nav_lower.innerText = newValue;
+// });
 
-		nodecg.listenFor('playTicker', () => {
-			if (netCBCRep.value == "true") {
-                // Play Audio In
-				var audio = document.getElementById("audioIn");
-  				audio.play();
-                // Animate In
-				console.log('playTicker');
-			} else {
-				console.log("CBC", netCBCRep.value);
-			}});
+// netCBCRep.on('change', (newValue) => {
+//     netCBCRep.value = newValue;
+//     console.log("CBC change", netCBCRep.value)
+// });	
 
-		nodecg.listenFor('stopTicker', () => {
-			if (netCBCRep.value == "true") {
-                // Play Audio Out
-				var audio = document.getElementById("audioOut");
-				audio.play();
-                // Animate Out
-				console.log("stopTicker");
-			} else {
-				console.log("CBC", netCBCRep.value);
-			}
-		});
+// nodecg.listenFor('playTicker', () => {
+//     if (netCBCRep.value == "true") {
+//         // Play Audio In
+//         var audio = document.getElementById("audioIn");
+//         audio.play();
+//         // Animate In
+//         console.log('playTicker');
+//     } else {
+//         console.log("CBC", netCBCRep.value);
+//     }});
+
+// nodecg.listenFor('stopTicker', () => {
+//     if (netCBCRep.value == "true") {
+//         // Play Audio Out
+//         var audio = document.getElementById("audioOut");
+//         audio.play();
+//         // Animate Out
+//         console.log("stopTicker");
+//     } else {
+//         console.log("CBC", netCBCRep.value);
+//     }
+// });
+
+function updateText(textClass, newText) {
+    const textElements = document.querySelectorAll(textClass);
+  
+    textElements.forEach((element) => {
+      element.classList.add('text-fade-out');
+  
+      element.addEventListener('animationend', () => {
+        element.textContent = newText;
+        element.classList.remove('text-fade-out');
+        element.classList.add('text-fade-in');
+  
+        element.addEventListener('animationend', () => {
+          element.classList.remove('text-fade-in');
+        }, { once: true });
+      }, { once: true });
+    });
+  }
+  
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const animateOutBtn = document.getElementById('animate-out-btn');
+    const animateInBtn = document.getElementById('animate-in-btn');
+    const updateTextBtn = document.getElementById('update-text-btn');
+  
+    animateOutBtn.addEventListener('click', () => {
+        const decoration = document.getElementById('ticker_decoration');
+        const main = document.getElementById('ticker_main');
+        main.style.animation = 'hide 1s ease-in forwards';
+      
+        main.addEventListener('animationend', () => {
+          decoration.style.animation = 'decoration-slide-up 1s ease-in forwards';
+        }, { once: true });
+      });
+      
+  
+    animateInBtn.addEventListener('click', () => {
+      const decoration = document.getElementById('ticker_decoration');
+      const main = document.getElementById('ticker_main');
+      decoration.style.animation = 'decoration-slide-down 1s ease-in forwards';
+      main.style.animation = 'reveal 1s ease-in forwards 1s';
+    });
+  
+    updateTextBtn.addEventListener('click', () => {
+      // Replace this with the actual new text you want to set
+      const newText = 'New Content';
+      updateText('.text', newText);
+    });
+  });
+  
+  
