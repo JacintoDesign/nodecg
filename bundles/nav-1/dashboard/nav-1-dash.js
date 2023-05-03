7d7954c0d46a0b0b5beeb67cb48069db12316dd6
Jacinto Wong <jacinto.wong@cbc.ca>
2023-05-03 14:20:45 -0400

    added ability to use array for preview, update and save to localStorage

--- bundles/nav-1/dashboard/nav-1-dash.js
+++ bundles/nav-1/dashboard/nav-1-dash.js
@@ -18,9 +18,6 @@ const time3Replicant = nodecg.Replicant('time-3');
 const footerReplicant = nodecg.Replicant('footer');
 
 headerReplicant.on('change', (newValue) => {
-    // The value of the Replicant has changed somewhere in NodeCG,
-    // this could be another dashboard panel, a server initiated change,
-    // or the doing of another user accessing your dashboard panel.
     headerInput.value = newValue;
 });
 titleReplicant.on('change', (newValue) => {
@@ -79,3 +76,119 @@ intervalBtn.onclick = () => {
     }
 };
 
+let channelInfos = [
+    { header: 'CBC', title: 'Alpine Skiing', time1: '11:30', time2: 'AM', time3: 'ET', day: 'Today' },
+    { header: 'TSN', title: 'Freestyle Halfpipe', time1: '8:00', time2: 'AM', time3: 'ET', day: 'Today' },
+    { header: 'SN', title: 'Ski Jumping', time1: '3:00', time2: 'PM', time3: 'ET', day: 'Tuesday' },
+    { header: 'TSN2', title: 'Biathalon', time1: '1:30', time2: 'PM', time3: 'ET', day: 'Friday' },
+    { header: 'SN1', title: 'Nordic Combined', time1: '11:00', time2: 'PM', time3: 'ET', day: 'Tonight' }
+];
+  
+// Add new variables for the arrow buttons
+const prevChannelBtn = document.getElementById('prev_channel');
+const nextChannelBtn = document.getElementById('next_channel');
+
+// Add a new variable to keep track of the current channel index
+let currentChannelIndex = 0;
+
+// Add a new function to update the text elements with the values from the channelInfos array
+function updateTextElements(channelInfo) {
+    headerInput.textContent = channelInfo.header;
+    titleInput.textContent = channelInfo.title;
+    time1.textContent = channelInfo.time1;
+    time2.textContent = channelInfo.time2;
+    time3.textContent = channelInfo.time3;
+    footerInput.textContent = channelInfo.day;
+}
+
+// Add a new function to update the channelInfos array with the current contenteditable values
+function updateChannelInfo() {
+    channelInfos[currentChannelIndex] = {
+        header: headerInput.textContent,
+        title: titleInput.textContent,
+        time1: time1.textContent,
+        time2: time2.textContent,
+        time3: time3.textContent,
+        day: footerInput.textContent
+    };
+    console.log(channelInfos[currentChannelIndex], channelInfos);
+}
+
+// Add input event listeners for contenteditable elements to update the channelInfos array
+headerInput.addEventListener('input', updateChannelInfo);
+titleInput.addEventListener('input', updateChannelInfo);
+time1.addEventListener('input', updateChannelInfo);
+time2.addEventListener('input', updateChannelInfo);
+time3.addEventListener('input', updateChannelInfo);
+footerInput.addEventListener('input', updateChannelInfo);
+
+// Add event listeners for the arrow buttons
+prevChannelBtn.onclick = () => {
+    currentChannelIndex--;
+    if (currentChannelIndex < 0) {
+        currentChannelIndex = channelInfos.length - 1;
+    }
+    updateTextElements(channelInfos[currentChannelIndex]);
+};
+
+nextChannelBtn.onclick = () => {
+    currentChannelIndex++;
+    if (currentChannelIndex >= channelInfos.length) {
+        currentChannelIndex = 0;
+    }
+    updateTextElements(channelInfos[currentChannelIndex]);
+};
+
+// Add a new variable for the add channel button
+const addChannelBtn = document.getElementById('add_channel_btn');
+
+// Add default values for new channels
+const defaultChannelInfo = {
+  header: 'Network',
+  title: 'Title',
+  time1: '12:00',
+  time2: 'AM',
+  time3: 'ET',
+  day: 'day'
+};
+
+// Update the addChannelBtn.onclick function to also save the updated array to localStorage
+addChannelBtn.onclick = () => {
+    channelInfos.push({ ...defaultChannelInfo });
+    currentChannelIndex = channelInfos.length - 1;
+    updateTextElements(channelInfos[currentChannelIndex]);
+    saveChannelInfos(); // Save the updated array to localStorage
+};
+
+// Add a function to save the channelInfos array to localStorage
+function saveChannelInfos() {
+    localStorage.setItem('channelInfos', JSON.stringify(channelInfos));
+}
+
+// Add a function to load the channelInfos array from localStorage if it exists
+function loadChannelInfos() {
+    const storedChannelInfos = localStorage.getItem('channelInfos');
+    if (storedChannelInfos) {
+        channelInfos = JSON.parse(storedChannelInfos);
+        updateTextElements(channelInfos[currentChannelIndex]);
+    }
+}
+
+// Update the updateChannelInfo function to also save the updated array to localStorage
+function updateChannelInfo() {
+    channelInfos[currentChannelIndex] = {
+        header: headerInput.innerText,
+        title: titleInput.innerText,
+        time1: time1.innerText,
+        time2: time2.innerText,
+        time3: time3.innerText,
+        day: footerInput.innerText
+    };
+    saveChannelInfos(); // Save the updated array to localStorage
+}
+  
+// Wrap the loadChannelInfos function call inside the DOMContentLoaded event listener
+document.addEventListener('DOMContentLoaded', () => {
+    loadChannelInfos();
+});
+  
